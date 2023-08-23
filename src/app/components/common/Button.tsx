92fdd4c0a0036b1f3662f5c3198269e2f5d64183
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-08-23 17:37:34 +0500

    add/hover color to the button component

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ className, onClick, children }) => {
   return (
     <button
       onClick={onClick}
-      className={`${className} w-full py-3 mt-10 bg-pink-500 text-white font-semibold text-xl tracking-wider rounded-md`}
+      className={`${className} w-full py-3 mt-10 bg-pink-500 text-white font-semibold text-xl tracking-wider rounded-md hover:bg-gray-900 transition-all duration-300`}
     >
       {children}
     </button>
